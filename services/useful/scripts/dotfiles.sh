e74704bd0e7ee981edc6fa2ba2745cfdf93e6141
matoruru <matoruru@shgt.jp>
2021-04-18 11:38:28 +0900

    Update scripts/dotfiles.sh

--- services/useful/scripts/dotfiles.sh
+++ services/useful/scripts/dotfiles.sh
@@ -5,6 +5,15 @@ cd ~/github/matoruru
 git clone https://github.com/matoruru/dotfiles
 cd dotfiles || exit 1
 
+git remote -v
+
+echo "Replace https with ssh"
+git remote -v | head -n1 | cut -f2 | cut -d' ' -f1 | \
+    sed 's/^https:\/\/github.com\//git@github.com:/' | \
+    xargs git remote set-url origin
+
+git remote -v
+
 echo ~/.bash_profile ~/.bashrc | xargs -n1 | xargs -I{} mv {} {}.old
 
 find .* -maxdepth 0 -type f | xargs -I{} ln -sr {} ~/
